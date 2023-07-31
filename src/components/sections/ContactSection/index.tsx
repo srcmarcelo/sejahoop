37a5625081ce88a6c579c76e6c82cd2069393881
Marcelo Cristian <marcelo.cristian.969@gmail.com>
2023-07-31 04:13:03 -0300

    fix text color

--- src/components/sections/ContactSection/index.tsx
+++ src/components/sections/ContactSection/index.tsx
@@ -6,10 +6,10 @@ const ContactSection: React.FC = () => {
     <div className='flex flex-col justify-center items-center pt-12 px-4'>
       <h1 className='text-secondary text-4.5xl mb-6'>CONTATO</h1>
       <div className='flex flex-col justify-center items-center text-center space-y-2 mb-6'>
-        <p className='text-lg font-light'>Email: info@hoopbank.com</p>
-        <p className='text-lg font-light'>Tel: (11) 3456-7890</p>
+        <p className='text-lg text-zinc-900 font-light'>Email: info@hoopbank.com</p>
+        <p className='text-lg text-zinc-900 font-light'>Tel: (11) 3456-7890</p>
       </div>
-      <p className='text-2xl font-light text-center max-w-3xl mb-2'>
+      <p className='text-2xl text-zinc-900 font-light text-center max-w-3xl mb-2'>
         Indique aqui a loja ou estabelecimento que você gostaria de comprar
         usando o Hoop
       </p>
